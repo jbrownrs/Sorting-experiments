609a599761875c4aa2ae9e362019f6a7e65f1ef9
jbrownrs <36203116+jbrownrs@users.noreply.github.com>
2018-02-25 17:07:43 +0000

    initial import of bubbleSort

---
+++ bubbleSort.py
@@ -0,0 +1,21 @@
+def bubbleSort(input):
+    length = len(input)
+    # Sort list to length - 1 as comparing one number with next
+    # will go off the end of the list otherwise
+    sortTo = length-1
+    sorted = False
+
+    while sorted != True :
+        sorted = True
+        # Go through the list until it is sorted, moving
+        # the biggest unsorted number to its final position
+        for x in range(0, sortTo) :
+            # swap numbers if left number is bigger
+            # than right number
+            if input[x] > input[x+1] :
+                temp = input[x]
+                input[x] = input[x+1]
+                input[x+1] = temp
+                sorted = False
+        sortTo = sortTo - 1
+    return input
